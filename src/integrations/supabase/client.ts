17d6bbf1251031c9a0ba5cf4c0c4350ae03741e4
Henrique Ferreira <henriqueanalista.ads@gmail.com>
2026-05-25 17:31:50 -0300

    update

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://aoarfvnmyjfjutgtcblh.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY =
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvYXJmdm5teWpmanV0Z3RjYmxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMDk5ODUsImV4cCI6MjA4OTU4NTk4NX0.mimw7aX6f3ZOpghitfEfXnL6Se9oqUhZyoLwAFjXvzo";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
